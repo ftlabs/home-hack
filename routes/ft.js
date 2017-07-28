fcdbde9de324c2c618682e8d17048104de89c4a9
Lily2point0 <lily.madar@ft.com>
2017-07-28 12:18:04 +0100

    comment out not needed stuff

--- routes/ft.js
+++ routes/ft.js
@@ -27,27 +27,24 @@ process.env.DEBUG = 'actions-on-google:*';
 
 //TODO: define custom actions here
 const Actions = {
-  FT_WELCOME: 'ft.welcome',
-  UNRECOGNIZED_DEEP_LINK: 'deeplink.unknown',
-  TELL_FACT: 'tell.fact',
-  TELL_CAT_FACT: 'tell.cat.fact'
+  FT_WELCOME: 'ft.welcome'
 };
 /** API.AI Parameters {@link https://api.ai/docs/actions-and-parameters#parameters} */
 //TODO: read on params?
-const Parameters = {
-  CATEGORY: 'category'
-};
-/** API.AI Contexts {@link https://api.ai/docs/contexts} */
-//TODO: add contexts + lifespans if needed
-const Contexts = {
-  FACTS: 'choose_fact-followup',
-  CATS: 'choose_cats-followup'
-};
-/** API.AI Context Lifespans {@link https://api.ai/docs/contexts#lifespan} */
-const Lifespans = {
-  DEFAULT: 5,
-  END: 0
-};
+// const Parameters = {
+//   CATEGORY: 'category'
+// };
+// /** API.AI Contexts {@link https://api.ai/docs/contexts} */
+// //TODO: add contexts + lifespans if needed
+// const Contexts = {
+//   FACTS: 'choose_fact-followup',
+//   CATS: 'choose_cats-followup'
+// };
+// /** API.AI Context Lifespans {@link https://api.ai/docs/contexts#lifespan} */
+// const Lifespans = {
+//   DEFAULT: 5,
+//   END: 0
+// };
 
 /**
  * @template T
@@ -114,18 +111,18 @@ if (!Object.values) {
  * Set up app.data for use in the action
  * @param {ApiAiApp} app ApiAiApp instance
  */
-const initData = app => {
-  /** @type {AppData} */
-  const data = app.data;
-  if (!data.facts) {
-    //TODO change object params below
-    data.facts = {
-      content: {},
-      cats: null
-    };
-  }
-  return data;
-};
+// const initData = app => {
+//   /** @type {AppData} */
+//   const data = app.data;
+//   if (!data.facts) {
+//     //TODO change object params below
+//     data.facts = {
+//       content: {},
+//       cats: null
+//     };
+//   }
+//   return data;
+// };
 
 /**
  * Say they've heard it all about this category
@@ -222,48 +219,48 @@ const initData = app => {
  * @param {ApiAiApp} app ApiAiApp instance
  * @return {void}
  */
-const tellCatFact = app => {
-  const data = initData(app);
-  if (!data.facts.cats) {
-    data.facts.cats = strings.cats.facts.slice();
-  }
-  const catFacts = data.facts.cats;
-  const fact = getRandomFact(catFacts);
-  /** @type {boolean} */
-  const screenOutput = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
-  if (!fact) {
-    // Add facts context to outgoing context list
-    app.setContext(Contexts.FACTS, Lifespans.DEFAULT, {});
-    // Replace outgoing cat-facts context with lifespan = 0 to end it
-    app.setContext(Contexts.CATS, Lifespans.END, {});
-    if (!screenOutput) {
-      return app.ask(strings.transitions.cats.heardItAll, strings.general.noInputs);
-    }
-    const richResponse = app.buildRichResponse()
-      .addSimpleResponse(strings.transitions.cats.heardItAll, strings.general.noInputs)
-      .addSuggestions(strings.general.suggestions.confirmation);
+// const tellCatFact = app => {
+//   const data = initData(app);
+//   if (!data.facts.cats) {
+//     data.facts.cats = strings.cats.facts.slice();
+//   }
+//   const catFacts = data.facts.cats;
+//   const fact = getRandomFact(catFacts);
+//   /** @type {boolean} */
+//   const screenOutput = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
+//   if (!fact) {
+//     // Add facts context to outgoing context list
+//     app.setContext(Contexts.FACTS, Lifespans.DEFAULT, {});
+//     // Replace outgoing cat-facts context with lifespan = 0 to end it
+//     app.setContext(Contexts.CATS, Lifespans.END, {});
+//     if (!screenOutput) {
+//       return app.ask(strings.transitions.cats.heardItAll, strings.general.noInputs);
+//     }
+//     const richResponse = app.buildRichResponse()
+//       .addSimpleResponse(strings.transitions.cats.heardItAll, strings.general.noInputs)
+//       .addSuggestions(strings.general.suggestions.confirmation);
 
-    return app.ask(richResponse);
-  }
-  const factPrefix = sprintf(strings.cats.factPrefix, getRandomValue(strings.cats.sounds));
-  if (!screenOutput) {
-    // <speak></speak> is needed here since factPrefix is a SSML string and contains audio
-    return app.ask(`<speak>${concat([factPrefix, fact, strings.general.nextFact])}</speak>`, strings.general.noInputs);
-  }
-  const image = getRandomValue(strings.cats.images);
-  const [url, name] = image;
-  const card = app.buildBasicCard(fact)
-    .setImage(url, name)
-    .addButton(strings.general.linkOut, strings.cats.link);
+//     return app.ask(richResponse);
+//   }
+//   const factPrefix = sprintf(strings.cats.factPrefix, getRandomValue(strings.cats.sounds));
+//   if (!screenOutput) {
+//     // <speak></speak> is needed here since factPrefix is a SSML string and contains audio
+//     return app.ask(`<speak>${concat([factPrefix, fact, strings.general.nextFact])}</speak>`, strings.general.noInputs);
+//   }
+//   const image = getRandomValue(strings.cats.images);
+//   const [url, name] = image;
+//   const card = app.buildBasicCard(fact)
+//     .setImage(url, name)
+//     .addButton(strings.general.linkOut, strings.cats.link);
 
-  const richResponse = app.buildRichResponse()
-    .addSimpleResponse(`<speak>${factPrefix}</speak>`)
-    .addBasicCard(card)
-    .addSimpleResponse(strings.general.nextFact)
-    .addSuggestions(strings.general.suggestions.confirmation);
+//   const richResponse = app.buildRichResponse()
+//     .addSimpleResponse(`<speak>${factPrefix}</speak>`)
+//     .addBasicCard(card)
+//     .addSimpleResponse(strings.general.nextFact)
+//     .addSuggestions(strings.general.suggestions.confirmation);
 
-  app.ask(richResponse, strings.general.noInputs);
-};
+//   app.ask(richResponse, strings.general.noInputs);
+// };
 
 
 const welcomeWithHeadlines = app => {
