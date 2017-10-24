aecf2855270bff9a62bb1652d1ca33307820ccd8
Lily2point0 <lily.madar@ft.com>
2017-10-24 14:12:12 +0100

    test quiz

--- routes/bbc.js
+++ routes/bbc.js
@@ -5,22 +5,39 @@ const router = express.Router();
 const path = require('path');
 
 const data = require('../public/data/articles.js');
+let currentArticle = 0;
 
 const { ApiAiApp } = require('actions-on-google');
 
 process.env.DEBUG = 'actions-on-google:*';
 
 const Actions = {
-  WELCOME: 'BBC.welcome'
+  WELCOME: 'BBC.welcome',
+  ASK: 'BBC.comment', 
 };
 
+const Context = {
+	ASK_LEAVE_COMMENT: 'leave_comment',
+	CHECK_QUIZ_ANWSER: 'check_quiz_answer'
+}
+
 const playWelcome = google => {
-	console.log('DATA::', data);
-	google.ask(`Hello Lily, you've read 3 stories today.`);
+	google.setContext('ASK_LEAVE_COMMENT');
+	google.ask(`Hello Lily, you've read 3 stories today. The last article you read is ${data[currentArticle].title}. Would you like to leave a comment on it?`);
+};
+
+const askQuiz = google => {
+	google.ask(`Great, to make sure you read the story, answer one question about the article correctly.`);
+};
+
+const matchAnswer = google => {
+	google.ask(`You gave the correct answer.`);
 };
 
 const actionMap = new Map();
 actionMap.set(Actions.WELCOME, playWelcome);
+actionMap.set(Actions.ASK, askQuiz);
+actionMap.set(Actions.ANSWER, matchAnswer);
 
 router.post('/', (request, response) => {
   const google = new ApiAiApp({ request, response });
