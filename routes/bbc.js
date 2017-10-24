cdc06b515cbfe880894e07d496698cc9b4812e49
Lily2point0 <lily.madar@ft.com>
2017-10-24 15:58:13 +0100

    add debug for fallback

--- routes/bbc.js
+++ routes/bbc.js
@@ -49,7 +49,8 @@ const askQuiz = google => {
 
 const matchAnswer = google => {
 	const userAnswer = google.getRawInput().toLowerCase();
-	console.log(userAnswer);
+	console.log('USER ANSWER::', userAnswer);
+	console.log('matchAnswer:::', google);
 	let reply = `Sorry that's not the correct answer, would you like to try another question?`;
 
 	if(userAnswer.startsWith(expectedAnswer.option.toLowerCase()) || userAnswer === expectedAnswer.value.toLowerCase()) {
