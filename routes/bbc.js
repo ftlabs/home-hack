4df00b0edaf9c708f5d8b8b9f086cafff6a867c4
Lily2point0 <lily.madar@ft.com>
2017-10-25 12:30:38 +0100

    debug 500

--- routes/bbc.js
+++ routes/bbc.js
@@ -38,8 +38,10 @@ const playWelcome = google => {
 	//TODO change context + add map action
 	console.log('PLAY WELCOME:::');
 	console.log(google);
-	google.ask(`<speak>Hi ${USER}, other readers are discussing the articles you red today. Would you like to talk about the last one you red, "${data[currentArticle].title}" <break time="0.5s" /> or another one?</speak>`);
 	google.setContext(Context.CHOOSE_ACTION, 1);
+	google.ask(`I'm saying something basic here`);
+	// google.ask(`<speak>Hi ${USER}, other readers are discussing the articles you red today. Would you like to talk about the last one you red, "${data[currentArticle].title}" <break time="0.5s" /> or another one?</speak>`);
+	// google.setContext(Context.CHOOSE_ACTION, 1);
 };
 
 const pickLastArticle = google => {
